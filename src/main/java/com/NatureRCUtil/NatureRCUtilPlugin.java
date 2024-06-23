d79f1880aa047c5f7a2eeeae37f8704be09bdc0e
alex-macm <45333464+alex-macm@users.noreply.github.com>
2024-06-23 22:18:55 +0100

    v1.0

--- src/main/java/com/NatureRCUtil/NatureRCUtilPlugin.java
+++ src/main/java/com/NatureRCUtil/NatureRCUtilPlugin.java
@@ -1,5 +1,32 @@
-package com.NatureRCUtil;
+/*
+ * Copyright (c) 2024, Alex MacManus <bricksterpepper@hotmailco.uk>
+ * Copyright (c) 2022, Jacob Petersen <jakepetersen1221@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
+
+
+package com.NatureRCUtil;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -21,8 +48,8 @@ import net.runelite.api.coords.WorldPoint;
 public class NatureRCUtilPlugin extends Plugin
 {
 	int[] capes = {
-			19476, //diary cape
-			13069 // diary cape (t)
+			ItemID.ACHIEVEMENT_DIARY_CAPE, //diary cape
+			ItemID.ACHIEVEMENT_DIARY_CAPE_T // diary cape (t)
 	};
 	@Inject
 	private Client client;
